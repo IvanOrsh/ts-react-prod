21ec0108d4ff1deb31644e23b1d0e758855cd956
IvanOrsh <ksironov@mail.ru>
2023-07-28 00:15:13 +0300

    feat: wire App and NavBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import { Link } from "react-router-dom";
 
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App = () => {
@@ -14,15 +12,10 @@ const App = () => {
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+
+      <AppRouter />
     </div>
   );
 };
