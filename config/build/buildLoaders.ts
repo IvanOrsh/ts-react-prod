b05310f763ebceca52ac9dbaabcd23d132ba41d7
IvanOrsh <ksironov@mail.ru>
2023-07-27 19:49:43 +0300

    fix: gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,40 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { RuleSetRule } from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders(options: BuildOptions): RuleSetRule[] {
+  const fileLoader: RuleSetRule = {
+    test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+    type: "asset/resource",
+  };
+
+  const cssLoader: RuleSetRule = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes(".module."),
+            localIdentName: options.isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader: RuleSetRule = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, fileLoader, cssLoader];
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import { Configuration } from "webpack";
+
+import { BuildOptions } from "./types/config";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths, isDev } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+
+    devServer: isDev ? buildDevServer(options) : undefined,
+    devtool: isDev ? "inline-source-map" : undefined,
+
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    plugins: buildPlugins(options),
+  };
+}
