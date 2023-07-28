979ca1f95f7a6033c359de5591b0e42d4630206b
IvanOrsh <ksironov@mail.ru>
2023-07-28 13:52:51 +0300

    refactor: remove ThemeSwitcher from Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
 import cls from "./Navbart.module.scss";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavBarProps {
   className?: string;
@@ -11,7 +10,6 @@ interface NavBarProps {
 export const Navbar = ({ className }: NavBarProps) => {
   return (
     <div className={classNames(cls.navbar)}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
