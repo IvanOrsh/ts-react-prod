dfc6936cf23b363b63c45b2c1e98cc80930545e3
IvanOrsh <ksironov@mail.ru>
2023-07-29 23:54:15 +0300

    feat: set up ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,16 @@ import App from "app/App";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 
 import "shared/config/i18n/i18n";
+import { ErrorBoundary } from "app/providers/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
