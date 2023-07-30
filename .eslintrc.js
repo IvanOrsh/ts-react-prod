e19436f5e7aedf87d10c2cf0be766a188f0c3673
IvanOrsh <ksironov@mail.ru>
2023-07-31 01:15:27 +0300

    chore: install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:i18next/recommended",
-    "plugin:react/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:i18next/recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
+  overrides: [{
+    env: {
+      node: true
     },
-  ],
+    files: [".eslintrc.{js,cjs}"],
+    parserOptions: {
+      sourceType: "script"
+    }
+  }],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["@typescript-eslint", "react", "i18next", "prettier"],
   rules: {
@@ -32,24 +25,20 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
-
     // note you must disable the base rule as it can report incorrect errors
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn", // or "error"
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
-
+    "@typescript-eslint/no-unused-vars": ["warn",
+    // or "error"
+    {
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+      caughtErrorsIgnorePattern: "^_"
+    }],
     // 'React' must be in scope when using JSX
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-  },
+    "react/jsx-filename-extension": [1, {
+      extensions: [".js", ".jsx", ".ts", ".tsx"]
+    }]
+  }
 };
