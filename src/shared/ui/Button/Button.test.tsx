37979943e54df52c8d68e28cb81abf3d547b9229
IvanOrsh <ksironov@mail.ru>
2023-07-30 22:27:25 +0300

    test: ensure Button renders correctly

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import { Button, ThemeButton } from "./Button";
+
+describe("<Button>", () => {
+  test("should render button with given text", () => {
+    render(<Button>TEST</Button>);
+    expect(screen.getByText("TEST")).toBeInTheDocument();
+  });
+
+  test("should render button with 'clear' in its class name", () => {
+    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+    expect(screen.getByText("TEST")).toHaveClass("clear");
+  });
+});
