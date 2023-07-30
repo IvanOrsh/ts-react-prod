4016014b28993e6b3c9a17e1bc25fb55c58de26a
IvanOrsh <ksironov@mail.ru>
2023-07-31 02:13:06 +0300

    refactor: extract css loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { RuleSetRule } from "webpack";
 import { BuildOptions } from "./types/config";
+import { buildCssLoaders } from "./loaders/buildCssLoaders";
 
 export function buildLoaders(options: BuildOptions): RuleSetRule[] {
   const svgLoader: RuleSetRule = {
@@ -34,27 +34,7 @@ export function buildLoaders(options: BuildOptions): RuleSetRule[] {
     },
   };
 
-  const cssLoader: RuleSetRule = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => resPath.includes(".module."),
-            localIdentName: options.isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      // Compiles Sass to CSS
-      "sass-loader",
-    ],
-  };
+  const cssLoader: RuleSetRule = buildCssLoaders(options.isDev);
 
   const typescriptLoader: RuleSetRule = {
     test: /\.tsx?$/,
