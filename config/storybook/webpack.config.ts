ad5a6b43c1a98e160c962b06b4ee24b76f40c742
IvanOrsh <ksironov@mail.ru>
2023-07-31 19:06:16 +0300

    chore: set up svg loader for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -2,8 +2,9 @@ import path from "path";
 import { Configuration } from "webpack";
 
 import { BuildPaths } from "../build/types/config";
-import { buildCssLoader } from "../build/loaders/buildCssLoader";
 import { buildFileLoader } from "../build/loaders/buildFileLoader";
+import { buildCssLoader } from "../build/loaders/buildCssLoader";
+import { buildSvgLoader } from "../build/loaders/buildSvgLoader";
 
 export default ({ config }: { config: Configuration }) => {
   const paths: BuildPaths = {
@@ -12,9 +13,25 @@ export default ({ config }: { config: Configuration }) => {
     entry: "",
     src: path.resolve(__dirname, "..", "..", "src"),
   };
+
+  // This modifies the existing image rule to exclude '.svg' files
+  // since we handle those with '@svgr/webpack' (in buildSvgLoader)
+  // solution copied from https://github.com/storybookjs/storybook/issues/18557
+  const imageRule = config.module?.rules?.find((rule) => {
+    if (rule && typeof rule !== "string" && rule.test instanceof RegExp) {
+      return rule.test.test(".svg");
+    }
+  });
+  if (imageRule && typeof imageRule !== "string") {
+    imageRule.exclude = /\.svg$/;
+  }
+
   config.resolve!.modules!.push(paths.src);
-  config.resolve!.extensions!.push(".ts", "tsx");
+  config.module!.rules!.push(buildSvgLoader());
   config.module!.rules!.push(buildFileLoader());
+  config.resolve!.extensions!.push(".ts", "tsx");
+
   config.module!.rules!.push(buildCssLoader(true));
+
   return config;
 };
