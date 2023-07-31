7858feb95b53ad7a41807959d3bd71d60d411b54
IvanOrsh <ksironov@mail.ru>
2023-07-31 19:09:11 +0300

    feat: set up global decorators for storybook

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,6 +1,10 @@
 import type { Preview } from "@storybook/react";
 
+import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+
 const preview: Preview = {
+  decorators: [StyleDecorator, RouterDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
