4a13b6eba731bfc0846d2003f6291eeb13123d8f
IvanOrsh <ksironov@mail.ru>
2023-08-01 19:34:10 +0300

    feat: improve theme provider

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -2,17 +2,23 @@ import { PropsWithChildren, useMemo, useState } from "react";
 
 import { ThemeContext, Theme } from "../lib/ThemeContext";
 
+interface ThemeProviderProps {
+  initialTheme?: Theme;
+}
+
 const defaultTheme = (localStorage.getItem("theme") as Theme) || Theme.LIGHT;
 
-const ThemeProvider = ({ children }: PropsWithChildren) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme);
+const ThemeProvider = (props: PropsWithChildren<ThemeProviderProps>) => {
+  const { initialTheme, children } = props;
+
+  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
   const defaultProps = useMemo(
     () => ({
       theme,
       setTheme,
     }),
-    [theme]
+    [theme],
   );
 
   return (
