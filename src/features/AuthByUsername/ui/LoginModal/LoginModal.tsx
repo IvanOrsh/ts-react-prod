56cb8a0b5fa4b9afe3a69dcabdeb3f65ce53b61f
IvanOrsh <ksironov@mail.ru>
2023-08-03 16:18:19 +0300

    feat: login modal for auth feature

---
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -0,0 +1,16 @@
+import { Modal } from "shared/ui/Modal/Modal";
+import { LoginForm } from "../LoginForm/LoginForm";
+
+interface LoginModalProps {
+  className?: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} lazy>
+      <LoginForm />
+    </Modal>
+  );
+};
