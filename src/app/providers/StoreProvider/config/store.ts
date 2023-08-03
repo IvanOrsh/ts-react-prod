4da67b997594ba7a044a683b07a856e3ea5e17f5
IvanOrsh <ksironov@mail.ru>
2023-08-03 16:17:09 +0300

    feat: user slice for auth

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,16 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { ReducersMapObject, configureStore } from "@reduxjs/toolkit";
 import { StateSchema } from "./StateSchema";
 import { counterReducer } from "entities/Counter";
+import { userReducer } from "entities/User";
 
 export function createReduxStore(initialState?: StateSchema) {
   // <S = any, A extends Action = AnyAction, M extends Middlewares<S>
   // state
   // action
   // middleware
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer,
+  };
+
   return configureStore<StateSchema>({
-    reducer: {
-      counter: counterReducer,
-    },
+    reducer: rootReducers,
     preloadedState: initialState,
     devTools: __IS_DEV__,
   });
