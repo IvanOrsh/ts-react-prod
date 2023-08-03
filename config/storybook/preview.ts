b5d18ca17a0d5fa74029f73fb7a14111bc31e051
IvanOrsh <ksironov@mail.ru>
2023-08-04 02:18:17 +0300

    fix: remove translation decorator as it is disrupting loki

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -2,10 +2,9 @@ import type { Preview } from "@storybook/react";
 
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
-import { TranslationDecorator } from "../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator";
 
 const preview: Preview = {
-  decorators: [StyleDecorator, RouterDecorator, TranslationDecorator],
+  decorators: [StyleDecorator, RouterDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
