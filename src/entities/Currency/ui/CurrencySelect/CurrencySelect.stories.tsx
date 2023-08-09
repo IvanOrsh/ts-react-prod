20abf4221c7fd272649a81ead127988364c6230c
IvanOrsh <ksironov@mail.ru>
2023-08-09 17:14:48 +0300

    feat: currency entity with CurrencySelect component

---
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { CurrencySelect } from "./CurrencySelect";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+
+const meta = {
+  title: "entities/CurrencySelect",
+  component: CurrencySelect,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [ThemeDecorator(Theme.LIGHT)],
+} satisfies Meta<typeof CurrencySelect>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
+
+export const Dark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
