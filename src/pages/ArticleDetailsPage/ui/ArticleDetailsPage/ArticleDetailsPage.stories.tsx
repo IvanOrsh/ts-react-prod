e357f9ff11f0cc42d3e09cff608ca4f5a30cf712
IvanOrsh <ksironov@mail.ru>
2023-08-12 03:16:51 +0300

    feat: article details page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ArticleDetailsPage from "./ArticleDetailsPage";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+
+const meta = {
+  title: "pages/ArticleDetailsPage",
+  component: ArticleDetailsPage,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [ThemeDecorator(Theme.LIGHT)],
+} satisfies Meta<typeof ArticleDetailsPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
