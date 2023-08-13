225cfab9ac557fbb474296d846158ca424550507
IvanOrsh <ksironov@mail.ru>
2023-08-13 03:32:01 +0300

    chore: update store decorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -5,10 +5,12 @@ import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
 import { ReducersList } from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 import { profileReducer } from "entities/Profile";
 import { loginReducer } from "features/AuthByUsername";
+import { articleDetailsReducer } from "entities/Article";
 
 const defaultAsyncReducers: ReducersList = {
   loginForm: loginReducer,
   profile: profileReducer,
+  articleDetails: articleDetailsReducer,
 };
 
 export const StoreDecorator =
