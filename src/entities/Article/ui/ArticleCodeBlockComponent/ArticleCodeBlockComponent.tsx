d8fdd01e715e4478e5eb3c2a23f5718f4dc98db8
IvanOrsh <ksironov@mail.ru>
2023-08-15 03:01:13 +0300

    fix: styles

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -4,6 +4,8 @@ import { Code } from "shared/ui/Code/Code";
 import { ArticleCodeBlock } from "../../model/types/article";
 import { classNames } from "shared/lib/classNames/classNames";
 
+import cls from "./ArticleCodeBlockComponent.module.scss";
+
 interface ArticleCodeBlockComponentProps {
   className?: string;
   block: ArticleCodeBlock;
@@ -12,7 +14,9 @@ interface ArticleCodeBlockComponentProps {
 export const ArticleCodeBlockComponent = memo(
   ({ className, block }: ArticleCodeBlockComponentProps) => {
     return (
-      <div className={classNames("", {}, [className])}>
+      <div
+        className={classNames(cls.ArticleCodeBlockComponent, {}, [className])}
+      >
         <Code text={block.code} />
       </div>
     );
