22259d6228b5a26e6074e64bc491be4638db289a
IvanOrsh <ksironov@mail.ru>
2023-08-17 00:44:54 +0300

    feat: articleDetailsComments: service, schema, ui

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,1 +1,10 @@
 export { ArticleDetailsPageAsync as ArticleDetailsPage } from "./ui/ArticleDetailsPage/ArticleDetailsPage.async";
+
+export { ArticleDetailsCommentsSchema } from "./model/types/ArticleDetailsCommentsSchema";
+
+export {
+  articleDetailsCommentsActions,
+  articleDetailsCommentsReducer,
+  articleDetailsCommentsSlice,
+  getArticleComments,
+} from "./model/slices/articleDetailsCommentsSlice";
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,27 +3,64 @@ import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 import { ArticleDetails } from "entities/Article";
+import {
+  articleDetailsCommentsReducer,
+  getArticleComments,
+} from "../../model/slices/articleDetailsCommentsSlice";
+import {
+  // getArticleCommentsError,
+  getArticleCommentsIsLoading,
+} from "../../model/selectors/comments";
+import {
+  useAppDispatch,
+  useAppSelector,
+  useInitialEffect,
+} from "shared/lib/hooks";
+import { CommentList } from "entities/Comment";
+import { Text } from "shared/ui/Text/Text";
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 import { classNames } from "shared/lib/classNames/classNames";
+
+import cls from "./ArticleDetailsPage.module.scss";
+import { fetchCommentsByArticleId } from "pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId";
 
 interface ArticleDetailsPageProps {
   className?: string;
 }
 
+const reducers: ReducersList = {
+  articleDetailsComments: articleDetailsCommentsReducer,
+};
+
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   const { t } = useTranslation("article-details");
   const { id } = useParams<{ id: string }>();
+  const dispatch = useAppDispatch();
+
+  const comments = useAppSelector(getArticleComments.selectAll);
+  const isLoading = useAppSelector(getArticleCommentsIsLoading);
+  // const error = useAppSelector(getArticleCommentsError);
+
+  useInitialEffect(() => dispatch(fetchCommentsByArticleId(id)));
 
   if (!id) {
     return (
-      <div className={classNames("", {}, [className])}>
+      <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
         {t("Статья не найдена")}
       </div>
     );
   }
   return (
-    <div className={classNames("", {}, [className])}>
-      <ArticleDetails id={id!} />
-    </div>
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <div className={classNames("", {}, [className])}>
+        <ArticleDetails id={id!} />
+        <Text className={cls.commentTile} title={t("Комментарии")} />
+        <CommentList isLoading={isLoading} comments={comments} />
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
