77d3b25f168239d7c91130e8f00a3c4670fd6b02
IvanOrsh <ksironov@mail.ru>
2023-08-18 21:29:00 +0300

    fix: make comment prop optional, styles for loading state

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -1,27 +1,60 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { CommentCard } from "./CommentCard";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
+
 const meta = {
-  title: "/CommentCard",
+  title: "entities/Comment/CommentCard",
   component: CommentCard,
-  parameters: {
-    layout: "centered",
-  },
   tags: ["autodocs"],
   args: {
     comment: {
       id: "1",
       text: "you are fired",
       user: {
-        id: "1",
-        username: "admin",
+        id: "2",
+        username: "user",
+        avatar:
+          "https://images.unsplash.com/photo-1616766098946-e4fabb7d6da0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
       },
-      // articleId: "1",
     },
   },
   decorators: [ThemeDecorator(Theme.LIGHT)],
 } satisfies Meta<typeof CommentCard>;
+
 export default meta;
+
 type Story = StoryObj<typeof meta>;
+
 export const Primary: Story = {};
+export const Dark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
+export const Loading: Story = {
+  args: {
+    comment: {
+      id: "1",
+      text: "hello",
+      user: {
+        id: "1",
+        username: "admin",
+      },
+    },
+    isLoading: true,
+  },
+};
+export const LoadingDark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+  args: {
+    comment: {
+      id: "1",
+      text: "hello",
+      user: {
+        id: "1",
+        username: "admin",
+      },
+    },
+    isLoading: true,
+  },
+};
--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -6,13 +6,13 @@ import { Avatar } from "shared/ui/Avatar/Avatar";
 import { Text } from "shared/ui/Text/Text";
 import { Skeleton } from "shared/ui/Skeleton/Skeleton";
 import { classNames } from "shared/lib/classNames/classNames";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
 
 import cls from "./CommentCard.module.scss";
-import { RoutePath } from "shared/config/routeConfig/routeConfig";
 
 interface CommentCardProps {
   className?: string;
-  comment: Comment;
+  comment?: Comment;
   isLoading?: boolean;
 }
 
@@ -21,7 +21,9 @@ export const CommentCard = memo((props: CommentCardProps) => {
 
   if (isLoading) {
     return (
-      <div className={classNames(cls.CommentCard, {}, [className])}>
+      <div
+        className={classNames(cls.CommentCard, {}, [className, cls.loading])}
+      >
         <div className={cls.header}>
           <Skeleton width={30} height={30} border="50%" />
           <Skeleton width={100} height={16} className={cls.username} />
@@ -29,6 +31,10 @@ export const CommentCard = memo((props: CommentCardProps) => {
         <Skeleton width="100%" height={50} className={cls.text} />
       </div>
     );
+  }
+
+  if (!comment) {
+    return null;
   }
 
   return (
